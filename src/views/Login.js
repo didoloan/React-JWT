c3170da16e09832f5db9e1a78291060f0e7c2fba
Doyin Awosanya <didoloan@gmail.com>
2021-02-12 21:06:27 +0100

    fixed error preventing bug

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
 import { apiBaseURL } from '../app.json';
-import { useDispatch, useSelector, connect, dispatch } from 'react-redux';
+import { useDispatch, useSelector, connect } from 'react-redux';
 import { Link, navigate } from '@reach/router';
 import { GoogleLogin } from 'react-google-login';
 import FacebookLogin from 'react-facebook-login';
