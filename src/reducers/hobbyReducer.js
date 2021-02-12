a4e59957daea040443c2ebf5cda079d1cff19350
Doyin Awosanya <didoloan@gmail.com>
2021-02-12 22:42:13 +0100

    fixed error preventing bug

--- src/reducers/hobbyReducer.js
+++ src/reducers/hobbyReducer.js
@@ -3,7 +3,7 @@ import { INIT_HOBBY, ADD_HOBBY, DELETE_HOBBY } from '../actions/hobbyActions'
 const hobbyReducer = (initial=[], action) => {
     switch(action.type) {
         case INIT_HOBBY:
-            return action.datalist;
+            return action.dataList;
         case ADD_HOBBY:
             return [...initial, ...action.dataList]
         case DELETE_HOBBY:
